c29e63a78ec8acba1ed96a74905a2281819b3183
minngaeng <124495210+minngaeng@users.noreply.github.com>
2023-06-02 19:16:03 +0900

    [230602] 공배수.js

--- 프로그래머스/unrated/181936. 공배수/공배수.js
+++ 프로그래머스/unrated/181936. 공배수/공배수.js
@@ -6,3 +6,12 @@ function solution(number, n, m) {
   }
    
 }
+
+//다른 사람 풀이
+
+
+function solution(number, n, m) {
+    return (number%n ===0) ? (number%m===0) ? 1 : 0 : 0;
+}
+
+//이렇게 쓴 삼항연산자는 처음 봤다.. 오늘의 공부.... 미친거 아이고!!!!
