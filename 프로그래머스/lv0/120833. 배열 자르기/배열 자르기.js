8a2c60f3f7fa170d514daae2ac28729e2a63e96b
minngaeng <124495210+minngaeng@users.noreply.github.com>
2023-06-03 21:54:57 +0900

    [level 0] Title: 배열 자르기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120833. 배열 자르기/배열 자르기.js
@@ -0,0 +1,17 @@
+// function solution(numbers, num1, num2) {
+//     // var answer = [];
+//     for(let i =0; i < numbers.length; i++){
+//       if( numbers[i] === num1){
+//        numbers.slice
+//       }
+//     }
+    
+//     return answer;
+// }
+
+const solution = (numbers,num1,num2) => {
+
+  let arr = numbers.slice(num1,num2 + 1)
+  
+  return arr
+}
