119bf38b4e381851ee00e9b01d1ec2ff4bf96c18
minngaeng <124495210+minngaeng@users.noreply.github.com>
2023-06-12 15:32:47 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -1,12 +1,22 @@
+// function solution(num_list) {
+//     var evenNum = []; //짝수
+//     var oddNum = [] //홀수
+//     // return answer;
+//     for (let i = 0; i < num_list.length; i++) {
+//         if (num_list[i] % 2 === 0) {
+//             evenNum.push(num_list[i])
+//         } else {
+//             oddNum.push(num_list[i])
+//         }
+//     } return [ evenNum.length, oddNum.length]  // [짝수의 개수, 홀수의 개수]
+// }
+
+
 function solution(num_list) {
-    var evenNum = []; //짝수
-    var oddNum = [] //홀수
-    // return answer;
-    for (let i = 0; i < num_list.length; i++) {
-        if (num_list[i] % 2 === 0) {
-            evenNum.push(num_list[i])
-        } else {
-            oddNum.push(num_list[i])
-        }
-    } return [ evenNum.length, oddNum.length]  // [짝수의 개수, 홀수의 개수]
+    return (
+    [
+        num_list.filter((num) => num % 2 === 0 ).length,
+        num_list.filter((num) => num % 2 === 1).length
+    ]
+    )
 }
