f7eb6e37b66660d36f32fddab96916df98ea686c
minngaeng <124495210+minngaeng@users.noreply.github.com>
2023-06-19 17:08:40 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.js
+++ 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.js
@@ -1,8 +1,3 @@
 function solution(array) {
-    // var answer = 0;
-    // return Math.floor(array.sort().length /2 )
     return array[Math.floor(array.sort((a, b) => a - b).length / 2)]
-
-    
-    
 }
