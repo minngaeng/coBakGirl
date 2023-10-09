febecb8b802a00c9c82a10aca4d35feaf016fdcb
minngaeng <124495210+minngaeng@users.noreply.github.com>
2023-10-10 02:35:30 +0900

    [level unrated] Title: 순서쌍의 개수, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120836. 순서쌍의 개수/순서쌍의 개수.js
@@ -0,0 +1,14 @@
+const solution = n => {
+    let count = 0;
+
+    for (let i = 1; i <= Math.sqrt(n); i++) {
+        if (n % i === 0) {
+            count++;
+            if (i !== n / i) {
+                count++;
+            }
+        }
+    }
+
+    return count;
+};
