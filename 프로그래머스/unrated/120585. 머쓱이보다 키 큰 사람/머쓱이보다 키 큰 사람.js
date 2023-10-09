12499465a00d55a588c287a60f24909a6b65a62a
minngaeng <124495210+minngaeng@users.noreply.github.com>
2023-10-10 00:18:18 +0900

    [level unrated] Title: 머쓱이보다 키 큰 사람, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
@@ -0,0 +1,10 @@
+
+const solution = (array, height) => {
+    let answer = []
+    for (let i = 0; i < array.length; i++){
+        if (array[i] > height) {
+            answer.push(array[i])
+        }
+    }
+    return answer.length 
+}
