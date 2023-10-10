87352a0b44f78f3b24f7d861452b4b58eb3e31c9
minngaeng <124495210+minngaeng@users.noreply.github.com>
2023-10-10 12:04:17 +0900

    [level unrated] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
+++ 프로그래머스/unrated/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
@@ -1,3 +1,5 @@
 
 
-const solution = my_string => my_string.replace(/[^0-9]/g,'').split('').map(Number).reduce((prev,cur)=>prev+cur,0)
+// const solution = my_string => my_string.replace(/[^0-9]/g,'').split('').map(Number).reduce((prev,cur)=>prev+cur,0)
+
+const solution = my_string => [...my_string].reduce((prev,cur)=>Number(cur) ? Number(cur) + prev : prev ,0)
