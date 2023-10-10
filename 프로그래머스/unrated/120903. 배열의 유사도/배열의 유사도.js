07d172a554b831e54f23883fa230b0b5a411e1ff
minngaeng <124495210+minngaeng@users.noreply.github.com>
2023-10-10 15:20:18 +0900

    [level unrated] Title: 배열의 유사도, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120903. 배열의 유사도/배열의 유사도.js
@@ -0,0 +1,9 @@
+
+const solution =(s1,s2) =>{
+    // const count = 0
+    // s1.reduce((prev,cur) => )
+    // const new =  [...s1, ...s2] 
+    // new.map((el) => el === new[])
+    return  s1.filter((el) => s2.includes(el)).length
+                             
+}
