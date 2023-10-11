556ccb99968b95b615497434094a168b96b5f1e1
minngaeng <124495210+minngaeng@users.noreply.github.com>
2023-10-11 18:21:48 +0900

    [level unrated] Title: 가위 바위 보, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120839. 가위 바위 보/가위 바위 보.js
+++ 프로그래머스/unrated/120839. 가위 바위 보/가위 바위 보.js
@@ -1,15 +1,25 @@
 const solution = rsp => {
-    const answer = []
-    const array = [...rsp]
-    for (let i = 0; i <  rsp.length; i++){
-        if (rsp[i] === '2'){
-             answer.push('0')
-        } else if (rsp[i] === '0'){
-             answer.push('5')
-        } else if (rsp[i] === '5'){
-             answer.push('2')
-        }
+    const object ={
+        2:0,
+        0:5,
+        5:2
     }
-    return answer.join('')
+   const answer = [...rsp].map((el) => object[el])
+                               return answer.join('')
+    
 }
+// const solution = rsp => {
+//     const answer = []
+//     const array = [...rsp]
+//     for (let i = 0; i <  rsp.length; i++){
+//         if (rsp[i] === '2'){
+//              answer.push('0')
+//         } else if (rsp[i] === '0'){
+//              answer.push('5')
+//         } else if (rsp[i] === '5'){
+//              answer.push('2')
+//         }
+//     }
+//     return answer.join('')
+// }
                 
