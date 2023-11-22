b45ee5eb7f6c968d16e1dc8094ae45593bcb3819
minngaeng <124495210+minngaeng@users.noreply.github.com>
2023-11-23 05:16:52 +0900

    [level unrated] Title: 한 번만 등장한 문자, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
@@ -0,0 +1,22 @@
+function solution(s) {
+    var answer = '';
+    return answer;
+}function solution(s) {
+  let newArray = s.split('')
+  let result = {}
+  let newArray1=[]
+   for(let key of newArray){
+     result[key] = (result[key] || 0) + 1
+   }
+  
+  for (let key in result) {
+   if (result[key] === 1){
+     newArray1.push(key)
+   }
+     
+   
+     
+}
+  return newArray1.sort().join('')
+  
+}
