8ffe4485336ee575cb12a8a461e185249b9a2162
minngaeng <124495210+minngaeng@users.noreply.github.com>
2023-11-23 14:24:41 +0900

    [level unrated] Title: 문자열 계산하기, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120902. 문자열 계산하기/문자열 계산하기.js
@@ -0,0 +1,16 @@
+function solution(my_string) {
+        
+    const arr = my_string.split(' ')
+    
+    var answer = arr[0]*1;
+    
+    for(let i =1 ; i < arr.length; i++){
+        if(arr[i] ==='+'){
+            answer += arr[i+1]*1
+        }else if(arr[i]==='-'){
+            answer -= arr[i+1]*1
+        }
+    }
+
+    return answer;
+}
