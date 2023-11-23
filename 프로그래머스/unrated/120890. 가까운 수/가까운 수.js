4b88399c079a893bbe1650efa4ba3734e01f69ff
minngaeng <124495210+minngaeng@users.noreply.github.com>
2023-11-23 13:49:27 +0900

    [level unrated] Title: 가까운 수, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120890. 가까운 수/가까운 수.js
+++ 프로그래머스/unrated/120890. 가까운 수/가까운 수.js
@@ -1,10 +1,9 @@
 const solution = (array, n) => {
-    let differences = array.map(item => Math.abs(n - item));
-    let minDifference = Math.min(...differences);
 
-    // 최소 차이값을 가진 모든 인덱스를 찾음
-    let closestNumbers = array.filter((item, index) => differences[index] === minDifference);
-
-    // 가장 작은 수를 반환
-    return Math.min(...closestNumbers);
+    
+    let difference = array.map((el) => Math.abs(n-el))
+    let minDifference = Math.min(...difference)
+    
+    let closetNumbers= array.filter((el,index) => difference[index] ===minDifference )
+    return Math.min(...closetNumbers)
 };
